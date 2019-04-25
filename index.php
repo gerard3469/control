974aa37f49e19a039e0ec2c795134862a2f98d4b
unknown <gerard3469@hotmail.com>
2019-04-25 14:21:08 -0500

    Conexion

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
-echo "cambio";
+echo "conexion";
 
-/*
-		 Username: user 
-		 Password: 12345 
-		 Database Name: sampledb 
-		 Connection URL: mysql://salon:3306/
-	*/
+$enlace = mysqli_connect("localhost", "mi_usuario", "mi_contraseña", "world");
+
+/* comprobar la conexión */
+if (mysqli_connect_errno()) {
+    printf("Conexión fallida: %s\n", mysqli_connect_error());
+    exit();
+} else {
+
+	echo "conexion exitosa";
+}
+		/*
+		 Username: userMRA 
+		 Password: RIw4DP6FwQju05rD 
+		 Database Name: salon 
+		 Connection URL: mysql://mysql:3306/
+		 user root: 7FX3BtPqUOttX4iF
+
+		 */
 
 ?>
