2561d420c27e29c70fc49c1109076c5fe736ab64
unknown <gerard3469@hotmail.com>
2019-04-25 18:32:10 -0500

    sdas

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-echo "conexion";
+$servername = "mysql:3306";
+$database = "salon";
+$username = "userMRA";
+$password = "RIw4DP6FwQju05rD";
 
+// Create connection
 
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+echo "Connected successfully";
+mysqli_close($conn);
 		/*
 		 Username: userMRA 
 		 Password: RIw4DP6FwQju05rD 
