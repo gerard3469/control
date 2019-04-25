a9be83faa6c4095696cf260620f2f21ff3ac62fc
unknown <gerard3469@hotmail.com>
2019-04-24 22:59:21 -0500

    Modifique index conectar

--- index.php
+++ index.php
@@ -45,18 +45,8 @@
 </html>
 -->
 <?php
-	/*class  Db{
-		private static $conexion=NULL;
-		private function __construct (){}
- 
-		public static function conectar(){
-			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion= new PDO('mysql:172.30.125.213=:3306;dbname=sampledb','user','12345',$pdo_options);
-			return self::$conexion;
-		}		
-	}
-	*/
-	$enlace = mysqli_connect("salon:3306", "user", "12345", "sampledb");
+
+	$enlace = mysqli_connect("salon:3306", "user", "12uhkj345", "sampledb");
 
 /* comprobar la conexión */
 if (mysqli_connect_errno()) {
