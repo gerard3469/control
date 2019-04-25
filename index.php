d164a5d5e39d4c51aeba1192fde52ecb5c0e5bf1
unknown <gerard3469@hotmail.com>
2019-04-25 18:24:26 -0500

    Conexion

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 echo "conexion";
 
-$enlace = mysqli_connect("mysql://mysql:3306/", "userMRA", "RIw4DP6FwQju05rD", "salon");
 
-/* comprobar la conexión */
-if (mysqli_connect_errno()) {
-    printf("Conexión fallida: %s\n", mysqli_connect_error());
-    exit();
-} else {
-
-	echo "conexion exitosa";
-}
 		/*
 		 Username: userMRA 
 		 Password: RIw4DP6FwQju05rD 
