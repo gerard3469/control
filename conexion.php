32c7168d3e23b105924e2d8db0e26c855da7b1ee
unknown <gerard3469@hotmail.com>
2019-04-24 22:30:03 -0500

    Modifique la conexion al de la base de datos

--- conexion.php
+++ conexion.php
@@ -5,11 +5,16 @@
  
 		public static function conectar(){
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion= new PDO('mysql:host=localhost:3306;dbname=salon','user','12345',$pdo_options);
+			self::$conexion= new PDO('mysql:host=salon:3306;dbname=sampledb','user','12345',$pdo_options);
 			return self::$conexion;
 		}		
 	}
 	
-
+/*
+		 Username: user 
+		 Password: 12345 
+		 Database Name: sampledb 
+		 Connection URL: mysql://salon:3306/
+	*/
 
 ?>
