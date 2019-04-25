7afa4648668b411d69acc6109a4183ca2dfb2220
unknown <gerard3469@hotmail.com>
2019-04-25 18:53:34 -0500

    cambios

--- crud_alumno.php
+++ crud_alumno.php
@@ -9,6 +9,32 @@ require_once('conexion.php');
 		// método para insertar, recibe como parámetro un objeto de tipo alumno
 		public function insertar($alumno){
 			$db=Db::conectar();
+			$sql=<<<EOD
+				CREATE DATABASE IF NOT EXISTS salon;
+ 
+				USE salon;
+				CREATE TABLE `alumnos` (
+  					`id` int(11) NOT NULL,
+  					`nombre` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  					`apellido_paterno` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL ,
+  					`apellido_materno` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL ,
+  					`matricula` varchar(8) COLLATE utf8mb4_spanish2_ci NOT NULL
+  
+					) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+ 
+				ALTER TABLE `alumnos`
+  				ADD PRIMARY KEY (`id`);
+  
+				ALTER TABLE `alumnos`
+					ADD UNIQUE (`matricula`) ;
+    
+				ALTER TABLE `alumnos`
+  				MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+			EOD;
+
+			$crear = $db->prepare($sql);
+			$crear->execute();
+
 			$insert=$db->prepare('INSERT INTO alumnos values(NULL,:nombre,:app,:apm,:matricula)');
 			$insert->bindValue('nombre',$alumno->getNombre());
 			$insert->bindValue('app',$alumno->getApellido_paterno());
