a12274ee17df397fcd4c859423dead1e9f65054d
unknown <gerard3469@hotmail.com>
2019-04-24 23:01:30 -0500

    Modifique index conectar

--- index.php
+++ index.php
@@ -1,51 +1,5 @@
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Control alumnos</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-</head>
-<body>
-	
-
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<h1>Bienvenido al control de alumnos</h1>
-				</div>
-				<div class="col-12">
-					<table border=1 class="table table-borderless table-dark">			
-						<tr>
-							<td><a href="ingresar.php" class="btn btn-primary">Nuevo alumno <i class="fas fa-user-plus"></i></a></td>
-						</tr>
-						<tr>
-							<td><a href="mostrar.php" class="btn btn-primary">Ver todos <i class="fas fa-search"></i></a></td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			
-		</div>
-		<footer class="page-footer font-small blue">
-
-          <div class="footer-copyright text-center py-3">© Gerardo
-            <a href="https://mdbootstrap.com/education/bootstrap/">App cloud</a>
-          </div>
-
-
-        </footer>
-
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
--->
 <?php
-
+echo "Hola";
 	$enlace = mysqli_connect("salon:3306", "user", "12uhkj345", "sampledb");
 
 /* comprobar la conexión */
